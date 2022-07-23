6c7cf968c0e44e249f4f788095f1d183b5c7db27
Pavel Rudyakov <legion1983@narod.ru>
2022-07-24 00:24:32 +0300

    убрал повторы в функции, часть функций перенес в класс, использовал везде append

--- Main part of cursovaya/Main_code.py
+++ Main part of cursovaya/Main_code.py
@@ -2,18 +2,22 @@ import json
 import urllib.request
 import requests
 from pprint import pprint
+
 with open("token.txt", "r") as file_object:
     token = file_object.read().strip()
+
+
 # _________делаем запрос к api и вынимаем всю информацию_______
 class VkPhotoes:
     url = "https://api.vk.com/method/"
+
     def __init__(self, token, version):
         self.params = {
             "access_token": token,
             "v": version
         }
 
-    def get_photoes(self, count):
+    def get_photoes(self, count, q_ty_of_photos_max_size):
         photoes_url = self.url + "photos.get"
         photoes_params = {
             "album_id": "profile",
@@ -23,146 +27,94 @@ class VkPhotoes:
             "photo_sizes": 0
         }
         res = requests.get(photoes_url, params={**self.params, **photoes_params}).json()
-        return res["response"]
+        dict_short = {}
+
+# создаем специальный словарь dict_short, который сожержит только важную информацию о фотографиях (size, id, likes)
+        picture_list_size = []
+        picture_list_url = []
+        picture_list_date = []
+        picture_list_likes = []
+        picture_file_names = 'None'
+        for values in res["response"]["items"]:
+            # pprint(values)
+            for picture_size_date in values["sizes"]:
+                picture_list_size.append(picture_size_date['width'] * picture_size_date['height'])
+                picture_list_url.append(picture_size_date['url'])
+                length = len(picture_list_size)
+                if picture_size_date["url"] in picture_list_url:
+                    picture_list_date.append(values['date'])
+                    picture_list_likes.append(values['likes']["count"])
+                for z in range(0, length):
+                    dict_short[z] = dict(size=picture_list_size[z], date=picture_list_date[z], likes=picture_list_likes[z], file_names=picture_file_names, url=picture_list_url[z])
+        # pprint(dict_short)
+
+# ищем через сортировку фотографии с максимальным размером и их позиционный номер в нашем специальном словаре dict_short
+        list_max_size_pictures = []
+        for values in dict_short.values():
+            list_max_size_pictures.append((values['size']))
+        list_max_size_pictures_sorted = sorted(list_max_size_pictures)[-q_ty_of_photos_max_size:]
+        # print(list_max_size_pictures)
+
+        list_max_size_pictures_url = []
+        for values in dict_short.values():
+            for items in values.values():
+                if items in list_max_size_pictures_sorted:
+                    list_max_size_pictures_url.append(values['url'])
+
+# оставляет в словаре только значения с минимальными размерами фотографий
+        new_dict = {}
+        for key, value in dict_short.items():
+            if value["url"] in list_max_size_pictures_url:
+                new_dict[key] = value
+        # pprint(new_dict)
+
+        picture_names_for_new_dict = []
+        picture_date_new_dict = []
+
+        for values in new_dict.values():
+            picture_names_for_new_dict.append(values["likes"])
+            picture_date_new_dict.append(values["date"])
+
+# меняет имена фотографий, если они имеют одинаковое количество лайков
+        unique_names = []
+        unique_indexes = []
+        for number in picture_names_for_new_dict:
+            if number not in unique_names:
+                unique_names.append(number)
+            else:
+                unique_names.append(str(picture_names_for_new_dict[picture_names_for_new_dict.index(number)]) + "." + str(picture_date_new_dict[picture_names_for_new_dict.index(number)]))
+        # print(unique_names)
+        return [new_dict, unique_names]
+
 
 vk_client = VkPhotoes(token, "5.131")
-pprint(vk_client.get_photoes(count=22))
-
-raw_date_json = vk_client.get_photoes(count=22)
-
-
-picture_list_size = []
-picture_list_id = []
-picture_list_url = []
-picture_list_likes = []
-
-for values in raw_date_json["items"]:
-    for items in values["sizes"]:
-        picture_size = items['width'] * items['height']
-        picture_id = values["id"]
-        picture_url = items["url"]
-        picture_likes = (values["likes"]["count"])
-        picture_list_size += [picture_size]
-        picture_list_id += [picture_id]
-        picture_list_url += [picture_url]
-        picture_list_likes += [picture_likes]
-        sort_picture_list_size = sorted(picture_list_size)
-        top_5_size_sort_picture_list_size = sort_picture_list_size[-5:]
-
-list_max_size_position = []
-for numbers in top_5_size_sort_picture_list_size:
-    if numbers in picture_list_size:
-        x = [picture_list_size.index(numbers)]
-        list_max_size_position.extend(x)
-# print("Индексы  фотографий с наибольшим размером в списке фотографий", list_max_size_position)
-
-picture_dict_max = {}
-
-list_final_name_additional = []
-list_final_url = []
-list_final_name_1st = []
-
-for values in list_max_size_position:
-    file_name_additional = picture_list_id[values]
-    file_url = picture_list_url[values]
-    file_name_1st = picture_list_likes[values]
-
-    list_final_name_additional.append(file_name_additional)
-    list_final_url.append(file_url)
-    list_final_name_1st.append(file_name_1st)
-
-# __________Создание словаря с 5ю файлами фотографии наибольшого размера______________________
-
-def file_name_creation():
-    final_dict = {}
-
-    file_name_1 = list_final_name_1st[0]
-    file_name_2 = list_final_name_1st[1]
-    file_name_3 = list_final_name_1st[2]
-    file_name_4 = list_final_name_1st[3]
-    file_name_5 = list_final_name_1st[4]
-
-    file_name_additional_1 = list_final_name_additional[0]
-    file_name_additional_2 = list_final_name_additional[1]
-    file_name_additional_3 = list_final_name_additional[2]
-    file_name_additional_4 = list_final_name_additional[3]
-    file_name_additional_5 = list_final_name_additional[4]
-
-    list_final_url_1 = list_final_url[0]
-    list_final_url_2 = list_final_url[1]
-    list_final_url_3 = list_final_url[2]
-    list_final_url_4 = list_final_url[3]
-    list_final_url_5 = list_final_url[4]
-
-    if file_name_1 == file_name_2 or file_name_1 == file_name_3 or file_name_1 == file_name_4 or file_name_1 == file_name_5:
-        file_name_1 = str(file_name_1) + str("_") + str(file_name_additional_1)
-    else:
-        file_name_1 = str(file_name_1)
-
-    if file_name_2 == file_name_1 or file_name_2 == file_name_3 or file_name_2 == file_name_4 or file_name_2 == file_name_5:
-        file_name_2 = str(file_name_2) + str("_") + str(file_name_additional_2)
-    else:
-        file_name_2 = str(file_name_2)
-
-    if file_name_3 == file_name_1 or file_name_3 == file_name_2 or file_name_3 == file_name_4 or file_name_3 == file_name_5:
-        file_name_3 = str(file_name_3) + str("_") + str(file_name_additional_3)
-    else:
-        file_name_3 = str(file_name_3)
-
-    if file_name_4 == file_name_1 or file_name_4 == file_name_2 or file_name_4 == file_name_3 or file_name_4 == file_name_5:
-        file_name_4 = str(file_name_4) + str("_") + str(file_name_additional_4)
-    else:
-        file_name_4 = str(file_name_4)
-
-    if file_name_5 == file_name_1 or file_name_5 == file_name_2 or file_name_5 == file_name_3 or file_name_5 == file_name_4:
-        file_name_5 = str(file_name_5) + str("_") + str(file_name_additional_5)
-    else:
-        file_name_5 = str(file_name_5)
-
-    final_dict["file_1"] = [file_name_1, list_final_url_1]
-    final_dict["file_2"] = [file_name_2, list_final_url_2]
-    final_dict["file_3"] = [file_name_3, list_final_url_3]
-    final_dict["file_4"] = [file_name_4, list_final_url_4]
-    final_dict["file_5"] = [file_name_5, list_final_url_5]
-    pprint(final_dict)
-    return final_dict
-file_name_creation()
+urls = vk_client.get_photoes(count=11, q_ty_of_photos_max_size=5)
+pprint(urls)
 
 # ___________________________сохраняем информацию о файле в json формат_____________________
 with open('data.txt', 'w', encoding='utf-8') as f:
-    json.dump(file_name_creation(), f, ensure_ascii=False, indent=4)
+    json.dump(vk_client.get_photoes(count=11, q_ty_of_photos_max_size=5), f, ensure_ascii=False, indent=4)
 
 # __________Загрузка файлов на локальный диск______________________
-url = link_1 = (file_name_creation()['file_1'][1])
-url_picture_1 = str(file_name_creation()['file_1'][0])
-urllib.request.urlretrieve(url, 'C:/Users/daria/OneDrive/Рабочий стол/Нетология/cursovaya/' + url_picture_1 + ".jpg")
+def uploading_files():
+    lenght = len(urls[0].values())
+    url_list = []
+    for values in urls[0].values():
+        url = values["url"]
+        url_list.append(url)
+    for i in range(0, lenght):
+        picture_file_name = urls[1][i]
+        urllib.request.urlretrieve(url_list[i], 'C:/Users/daria/OneDrive/Рабочий стол/Нетология/cursovaya/' + str(picture_file_name) + ".jpg")
+uploading_files()
 
-url = link_2 = (file_name_creation()['file_2'][1])
-url_picture_2 = str(file_name_creation()['file_2'][0])
-urllib.request.urlretrieve(url, 'C:/Users/daria/OneDrive/Рабочий стол/Нетология/cursovaya/' + url_picture_2 + ".jpg")
-
-url = link_3 = (file_name_creation()['file_3'][1])
-url_picture_3 = str(file_name_creation()['file_3'][0])
-urllib.request.urlretrieve(url, 'C:/Users/daria/OneDrive/Рабочий стол/Нетология/cursovaya/' + url_picture_3 + ".jpg")
-
-url = link_4 = (file_name_creation()['file_4'][1])
-url_picture_4 = str(file_name_creation()['file_4'][0])
-urllib.request.urlretrieve(url, 'C:/Users/daria/OneDrive/Рабочий стол/Нетология/cursovaya/' + url_picture_4 + ".jpg")
-
-url = link_5 = (file_name_creation()['file_5'][1])
-url_picture_5 = str(file_name_creation()['file_5'][0])
-urllib.request.urlretrieve(url, 'C:/Users/daria/OneDrive/Рабочий стол/Нетология/cursovaya/' + url_picture_5 + ".jpg")
-
-#__________Загрузка файлов на yandex диск______________________
-
+# __________Загрузка файлов на yandex диск______________________
 URL = 'https://cloud-api.yandex.net/v1/disk/resources'
 TOKEN = "AQAAAAAAGUQeAADLW859Tk4gikBEmkURmI0u8H0"
 headers = {'Content-Type': 'application/json', 'Accept': 'application/json', 'Authorization': f'OAuth {TOKEN}'}
 
 def create_folder(path):
     requests.put(f'{URL}?path={path}', headers=headers)
-
-create_folder('Cursovaya')
+create_folder('VK_cursovaya')
 
 def upload_file(loadfile, savefile, replace=False):
     res = requests.get(f'{URL}/upload?path={savefile}&overwrite={replace}', headers=headers).json()
@@ -172,8 +124,8 @@ def upload_file(loadfile, savefile, replace=False):
         except KeyError:
             print(res)
 
-upload_file(r'C:/Users/daria/OneDrive/Рабочий стол/Нетология/cursovaya/' + url_picture_1 + ".jpg", "VK_cursovaya/" + url_picture_1)
-upload_file(r'C:/Users/daria/OneDrive/Рабочий стол/Нетология/cursovaya/' + url_picture_2 + ".jpg", "VK_cursovaya/" + url_picture_2)
-upload_file(r'C:/Users/daria/OneDrive/Рабочий стол/Нетология/cursovaya/' + url_picture_3 + ".jpg", "VK_cursovaya/" + url_picture_3)
-upload_file(r'C:/Users/daria/OneDrive/Рабочий стол/Нетология/cursovaya/' + url_picture_4 + ".jpg", "VK_cursovaya/" + url_picture_4)
-upload_file(r'C:/Users/daria/OneDrive/Рабочий стол/Нетология/cursovaya/' + url_picture_5 + ".jpg", "VK_cursovaya/" + url_picture_5)
+lenght = len(urls[0].values())
+for i in range(0, lenght):
+    picture_file_name = urls[1][i]
+    print(urls[1][i])
+    upload_file(r'C:/Users/daria/OneDrive/Рабочий стол/Нетология/cursovaya/' + str(picture_file_name) + ".jpg", "VK_cursovaya/" + str(picture_file_name))
